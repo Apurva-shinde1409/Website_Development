e2a00f8551111140d77680a51f1f85f7cbbd4f4f
Apurva Rajendra Shinde <101191938+Apurva-shinde1409@users.noreply.github.com>
2024-12-13 14:53:14 -0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Search Bar Interactivity
-document.getElementById("searchButton").addEventListener("click", () => {
-    const query = document.getElementById("searchInput").value;
-    alert(`Searching for: ${query}`);
-});
